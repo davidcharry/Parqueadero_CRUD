fedd26de97f323bd02ec59d33ed41a632ce3e222
David Charry <davidalejo1314@gmail.com>
2022-10-23 23:37:07 -0500

    Creación del archivo de modelo de Preguntas

---
+++ src/models/Mpreguntas.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose"); // importando el componente mogoose
+const PreguntasSchema = mongoose.Schema({
+    Opcion1: {
+        type: String,
+        required: true
+    },
+    Opcion2: {
+        type: String,
+        required: true
+    },
+    Opcion3: {
+        type: String,
+        required: true
+    },
+    Opcion4: {
+        type: String,
+        required: true
+    },
+    Res_Correcta: {
+        type: Number,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('MPreguntas', PreguntasSchema);
