b6f5e05e96ce870f061f8fa7508759afca60f514
David Charry <davidalejo1314@gmail.com>
2022-10-23 23:39:56 -0500

    Creación del endpoint de "Crear un nueva Pregunta" en MPreguntas

--- src/routes/Mpreguntas.js
+++ src/routes/Mpreguntas.js
@@ -1,3 +1,13 @@
 const express = require("express");
 const router = express.Router(); //manejador de rutas de express
 const PreguntasSchema = require("../models/Mpreguntas");
+
+
+//Crear un nuevo Usuario
+router.post("/MPreguntas", (req, res) => {
+    const Mpreguntas = PreguntasSchema(req.body);
+    Mpreguntas
+        .save()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
